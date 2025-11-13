d7fc59846428da7025bccc5d89ade2e747da18d4
ekashatakishvili <ekashatakishvili@gmail.com>
2025-11-13 21:28:12 +0400

    ფუტერი დავამატე და დიზაინი დავაპდეიტე

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
+//import { Component } from '@angular/core';
+//import { RouterOutlet } from '@angular/router';
+//import { HeaderComponent } from './header/header.component';
+
+//@Component({
+  //selector: 'app-root',
+  //imports: [RouterOutlet,HeaderComponent],
+  //templateUrl: './app.component.html',
+ // styleUrl: './app.component.scss'
+//})
+//export class AppComponent {
+  //title = 'finalproject';
+//}
+
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,HeaderComponent],
+  standalone: true,
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'finalproject';
+  title = 'finalprojeqt-main';
 }
